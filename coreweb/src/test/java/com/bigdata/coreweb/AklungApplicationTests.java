3f2a57a723e5214676dd060e8bcd950d0b570cd9
mikewang <1085262498@qq.com>
2020-02-03 18:12:28 +0800

    223

--- coreweb/src/test/java/com/bigdata/coreweb/AklungApplicationTests.java
+++ coreweb/src/test/java/com/bigdata/coreweb/AklungApplicationTests.java
@@ -8,7 +8,7 @@ class AklungApplicationTests {
 
 	@Test
 	void contextLoads() {
-		//111tet 44 453454435345565343543123123555555555afasdf
+		//111tet 44 453454435345565343543123123555555555afasdf345345
 	}
 
 }
